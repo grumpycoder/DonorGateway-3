847b4d437ee5c3e3717b46ac58c2bc27797cd863
Mark Lawrence <marklawrence72@gmail.com>
2016-07-01 13:31:56 -0500

    edit guest

---
+++ web/app/events/editGuest.controller.js
@@ -0,0 +1,36 @@
+﻿//mark.lawrence
+//editGuest.controller.js
+
+(function () {
+    'use strict';
+
+    var controllerId = 'EditGuestController';
+
+    angular.module('app.events').controller(controllerId, mainController);
+
+    mainController.$inject = ['logger', '$uibModalInstance', 'guestService', 'templateService', 'guest'];
+
+    function mainController(logger, $modal, service, templateService, guest) {
+        var vm = this;
+        vm.title = 'Edit Guest';
+
+        vm.guest = guest;
+
+        logger.log('guest', guest);
+
+        vm.cancel = function () {
+            $modal.dismiss();
+        }
+
+        vm.save = function () {
+            service.update(guest)
+                .then(function (data) {
+                    vm.guest = data;
+                    $modal.close(data);
+                }).finally(function () {
+                });
+        }
+    }
+
+
+})();
