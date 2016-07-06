5e901bd1223f7d6f5e43fa0dccc07c23ce857234
Mark Lawrence <marklawrence72@gmail.com>
2016-07-06 17:16:40 -0500

    edit guest wait queue if full

--- web/app/events/editGuest.controller.js
+++ web/app/events/editGuest.controller.js
@@ -14,6 +14,7 @@
         var vm = this;
         vm.title = 'Edit Guest';
         vm.event = event;
+        vm.isFull = false;
 
         vm.guest = angular.copy(guest);
 
@@ -21,9 +22,21 @@
             $modal.dismiss();
         }
 
+        vm.changeAttending = function() {
+            if (vm.guest.isAttending && vm.event.capacity < vm.event.registeredGuestCount + vm.guest.guestCount) {
+                vm.isFull = true;
+            } else {
+                vm.isFull = false; 
+            }
+        }
+
         vm.save = function () {
             //Check if first initialization of attending flag compared to original model
             if (vm.guest.isAttending && !guest.isAttending) vm.guest.responseDate = new Date();
+            if (vm.isFull) {
+                vm.guest.isWaiting = true;
+                vm.guest.waitingDate = new Date();
+            } 
             service.update(vm.guest)
                 .then(function (data) {
                     angular.extend(vm.guest, data);
